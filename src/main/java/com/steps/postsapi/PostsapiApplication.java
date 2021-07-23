55d63c47578d2ad221649cb113e48d61a1d530cb
Haim Gilboa <Haim.Gilboa@amdocs.com>
2021-07-23 16:28:14 +0300

    Initial posts api project

---
+++ src/main/java/com/steps/postsapi/PostsapiApplication.java
@@ -0,0 +1,13 @@
+package com.steps.postsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steps/postsapi/PostsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steps.postsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
